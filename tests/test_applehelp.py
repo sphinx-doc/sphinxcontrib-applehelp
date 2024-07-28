d51bb2b03d2ee12a997741626a79c0ad2f8716e7
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-07-28 05:02:27 +0100

    Adopt Ruff and use stricter MyPy settings

--- tests/test_applehelp.py
+++ tests/test_applehelp.py
@@ -1,12 +1,18 @@
 """Test for applehelp extension."""
 
-from pathlib import Path
+from __future__ import annotations
+
 import plistlib
+from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
 
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
 
-def check_structure(outdir):
+
+def check_structure(outdir: Path) -> None:
     contentsdir = outdir / 'Contents'
     assert contentsdir.is_dir()
     assert (contentsdir / 'Info.plist').is_file()
@@ -21,7 +27,7 @@ def check_structure(outdir):
     assert (contentsdir / 'Resources' / 'en.lproj').is_dir()
 
 
-def check_localization(outdir):
+def check_localization(outdir: Path) -> None:
     lprojdir = outdir / 'Contents' / 'Resources' / 'en.lproj'
     assert (lprojdir / 'localized.txt').is_file()
 
@@ -30,7 +36,7 @@ def check_localization(outdir):
     'applehelp', testroot='basic', srcdir='applehelp_output',
     confoverrides={'applehelp_bundle_id': 'org.sphinx-doc.Sphinx.help',
                    'applehelp_disable_external_tools': True})
-def test_applehelp_output(app, status, warning):
+def test_applehelp_output(app: Sphinx) -> None:
     LPROJ_DIR = Path(app.srcdir / 'en.lproj')
     LPROJ_DIR.mkdir(parents=True, exist_ok=True)
     LPROJ_DIR.joinpath('localized.txt').touch()
@@ -39,6 +45,6 @@ def test_applehelp_output(app, status, warning):
     # Have to use bundle_path, not outdir, because we alter the latter
     # to point to the lproj directory so that the HTML arrives in the
     # correct location.
-    bundle_path = Path(app.builder.bundle_path)
+    bundle_path = Path(app.builder.bundle_path)  # type: ignore[attr-defined]
     check_structure(bundle_path)
     check_localization(bundle_path)
