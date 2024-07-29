f4f9d900b238d03da601bf7c75cff5bcbcee6d7c
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-07-29 01:52:18 +0100

    Bump to 2.0.0

--- sphinxcontrib/applehelp/__init__.py
+++ sphinxcontrib/applehelp/__init__.py
@@ -32,8 +32,8 @@ else:
         progress_message,
     )
 
-__version__ = '1.0.8'
-__version_info__ = (1, 0, 8)
+__version__ = '2.0.0'
+__version_info__ = (2, 0, 0)
 
 package_dir = path.abspath(path.dirname(__file__))
 template_dir = path.join(package_dir, 'templates')
